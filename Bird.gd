7c764989ec0015efe765082840fb10675d0a38f3
Tran Uy Trieu Vi <tranuytrieuvi@gmail.com>
2022-06-20 21:00:03 +0700

    #1 reset flap implementation

--- Bird.gd
+++ Bird.gd
@@ -1,22 +1,16 @@
-extends KinematicBody2D
+extends RigidBody2D
 
-enum STATE { IDLE, FALL, JUMP }
 
-var vertical_vector = Vector2(0, 0)
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
 
-var gravity = 98
-var jump_speed = gravity * 1.5
 
-func _process(delta):
-    var state = STATE.FALL
-    if Input.is_action_just_pressed("ui_jump"):
-        state = STATE.JUMP
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    pass # Replace with function body.
 
-    match state:
-        STATE.FALL:
-            if vertical_vector.y < gravity * 1.5:
-                vertical_vector.y += gravity * 2 * delta
-        STATE.JUMP:
-            vertical_vector.y = -jump_speed
 
-    move_and_slide(vertical_vector)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#    pass
