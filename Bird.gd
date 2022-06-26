ed164c2b379d6e233909901d5eaa329fd3b6f0ea
Tran Uy Trieu Vi <tranuytrieuvi@gmail.com>
2022-06-26 22:23:51 +0700

    #1

--- Bird.gd
+++ Bird.gd
@@ -1,16 +1,7 @@
 extends RigidBody2D
 
+export var JUMP_HEIGHT = -150;
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-    pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#    pass
+func _process(delta):
+  if Input.is_action_just_pressed("ui_jump"):
+    self.set_linear_velocity(Vector2(0, JUMP_HEIGHT))
