b4ef4a6fe8d9216c76485c1541dc71c523334a29
Tran Uy Trieu Vi <tranuytrieuvi@gmail.com>
2022-07-08 22:05:05 +0700

    #3

--- Bird.gd
+++ Bird.gd
@@ -1,19 +1,4 @@
-# tool
-# extends RigidBody2D
-
-# export var JUMP_HEIGHT = -150
-# export var FLYING_SPEED = 100
-
-# func _ready():
-#     set_linear_velocity(Vector2(FLYING_SPEED, 0))
-# #    if not Engine.editor_hint:
-#     # TODO: remove this part. Maybe should be removed
-#     # self.position.x = get_viewport().get_visible_rect().size.x * 2 / 5
-#     # self.position.y = get_viewport().get_visible_rect().size.y / 2
-
-# func _process(delta):
-#     if Input.is_action_just_pressed("ui_jump"):
-#         set_linear_velocity(Vector2(FLYING_SPEED, JUMP_HEIGHT))
+tool
 extends KinematicBody2D
 
 export var FLYING_SPEED = 100
@@ -36,4 +21,7 @@ func _process(delta):
         STATE.JUMP:
             vertical_vector.y = JUMP_HEIGHT
 
-    move_and_slide(vertical_vector)
+    var collision = move_and_collide(vertical_vector * delta)
+
+    if collision:
+        print("I collided with ", collision.collider.name)
