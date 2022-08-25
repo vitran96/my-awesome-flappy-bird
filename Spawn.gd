bffba7df5a846c42dbe94108b103b5f74921c65e
vitran96 <tranuytrieuvi@gmail.com>
2022-08-25 21:46:03 +0700

    implement spawn

--- Spawn.gd
+++ Spawn.gd
@@ -1,7 +1,18 @@
 extends Area2D
 
+const Pipe := preload("res://Pipe.tscn")
+
+const NEW_PIPE_POSITION_X := 450
+const NEW_PIPE_POSITION_Y_RANGE_TOP := -60
+const NEW_PIPE_POSITION_Y_RANGE_BOTTOM := 60
 
 
+func pipe_reset() -> void:
+    var pipe_instance := Pipe.instance()
+    pipe_instance.position = Vector2(NEW_PIPE_POSITION_X, rand_range(NEW_PIPE_POSITION_Y_RANGE_TOP, NEW_PIPE_POSITION_Y_RANGE_BOTTOM))
+    get_tree().get_root().call_deferred("add_child", pipe_instance)
 
 func _on_Spawn_area_entered(area: Area2D) -> void:
-    print(area.name)
+    if area.name == "Pipes":
+        area.queue_free()
+        pipe_reset()
