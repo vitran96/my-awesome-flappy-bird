d68b56f461a9c0bb1bd49ac8b052020664513986
vitran96 <tranuytrieuvi@gmail.com>
2022-09-02 11:16:57 +0700

    #7

--- Spawn.gd
+++ Spawn.gd
@@ -6,10 +6,16 @@ const NEW_PIPE_POSITION_X := 450
 const NEW_PIPE_POSITION_Y_RANGE_TOP := -60
 const NEW_PIPE_POSITION_Y_RANGE_BOTTOM := 60
 
+func _ready() -> void:
+    # TODO: spawn 5 objects
+    pass
 
 func pipe_reset() -> void:
+    pipe_spawn(NEW_PIPE_POSITION_X)
+
+func pipe_spawn(x_position : int) -> void:
     var pipe_instance := Pipe.instance()
-    pipe_instance.position = Vector2(NEW_PIPE_POSITION_X, rand_range(NEW_PIPE_POSITION_Y_RANGE_TOP, NEW_PIPE_POSITION_Y_RANGE_BOTTOM))
+    pipe_instance.position = Vector2(x_position, rand_range(NEW_PIPE_POSITION_Y_RANGE_TOP, NEW_PIPE_POSITION_Y_RANGE_BOTTOM))
     get_tree().get_root().call_deferred("add_child", pipe_instance)
 
 func _on_Spawn_area_entered(area: Area2D) -> void:
