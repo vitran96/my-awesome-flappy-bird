2e5daca4bf24441b79bb080e16d79e218b059365
vitran96 <tranuytrieuvi@gmail.com>
2022-09-03 09:26:54 +0700

    #7

--- Bird.gd
+++ Bird.gd
@@ -7,6 +7,8 @@ extends KinematicBody2D
 
 #var vertical_vector := Vector2(FLYING_SPEED, 0)
 
+signal pass_pipe
+
 const UP := Vector2(0,-1)
 const FLAP := 200
 const MAX_FALL_SPEED = 200
@@ -23,3 +25,10 @@ func _physics_process(delta: float) -> void:
         motion.y = -FLAP
 
     motion = move_and_slide(motion, UP)
+    motion.x = 0
+
+func _on_Detect_area_entered(area: Area2D) -> void:
+    if area.name == "Score":
+        emit_signal("pass_pipe")
+    elif area.name == "Pipes":
+        get_tree().reload_current_scene()
---
+++ Hud.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+var score := 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    $ScoreBox/HBoxContainer/Score.text = str(score)
+
+func update_score():
+    score = score + 1
+    $ScoreBox/HBoxContainer/Score.text = str(score)
+
+func _on_Bird_pass_pipe() -> void:
+    update_score()
