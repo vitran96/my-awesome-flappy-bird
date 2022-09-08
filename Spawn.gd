35d16d10dd17e8d851bb2629bf807d7fb583541b
vitran96 <tranuytrieuvi@gmail.com>
2022-09-08 22:52:58 +0700

    #9

--- Spawn.gd
+++ Spawn.gd
@@ -9,7 +9,12 @@ const NEW_PIPE_POSITION_X := 270
 const NEW_PIPE_POSITION_Y_RANGE_TOP := -100
 const NEW_PIPE_POSITION_Y_RANGE_BOTTOM := 100
 
+var root: Node
+var main_node: Node
+
 func _ready() -> void:
+    root = get_tree().get_root()
+    main_node = root.get_child(0)
     clear_existing_pipe()
     spawn_new_pipes(NEW_PIPE_POSITION_X, PIPE_DISTANCE, START_PIPE_COUNT)
 
@@ -19,8 +24,6 @@ func spawn_new_pipes(position: int, distance: int, count: int) -> void:
         position = position + distance
 
 func clear_existing_pipe() -> void:
-    var root := get_tree().get_root()
-    var main_node := root.get_child(0)
     var nodes := main_node.get_children()
     var pipe_nodes := []
     for node in nodes:
@@ -39,7 +42,8 @@ func pipe_reset() -> void:
 func pipe_spawn(x_position : int) -> void:
     var pipe_instance := Pipe.instance()
     pipe_instance.position = Vector2(x_position, rand_range(NEW_PIPE_POSITION_Y_RANGE_TOP, NEW_PIPE_POSITION_Y_RANGE_BOTTOM))
-    get_tree().get_root().get_child(0).call_deferred("add_child", pipe_instance)
+    main_node.call_deferred("add_child", pipe_instance)
+    main_node.call_deferred("move_child", pipe_instance, 0)
 
 func _on_Spawn_area_entered(area: Area2D) -> void:
     if area.name == "Pipes":
